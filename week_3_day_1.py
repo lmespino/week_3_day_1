594c10bb8b7df7eae84f0f20eba496e69fb2b4e7
lmespino <lmespino@cps.edu>
2024-09-12 14:52:06 -0500

    variable naming

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -73,15 +73,15 @@ number = 1000
 #rint("Still, " + name + " was the best coder on the team and could write " + str(number) + " lines of code every day. Maybe " + song + " was " + name + "'s secret power?")
 
 
-number1 = 100
-number2 = 200
-number3 = 300
-number4 = 400
-number5 = 500
+# number1 = 100
+# number2 = 200
+# number3 = 300
+# number4 = 400
+# number5 = 500
 # put all of these into a sentence
 # print(str(number1) "," + str(number2), + str(number3), + str(number4), + str(number5))
 #f strings
-print(f"{number},{number1},{number2},{number3},{number4},{number5}")
+# print(f"{number},{number1},{number2},{number3},{number4},{number5}")
 ##########################################################################################
 
 
@@ -183,29 +183,33 @@ print(f"{number},{number1},{number2},{number3},{number4},{number5}")
 
 ##########################################################################################
 # #addition
+print(2+1)
 # #multiplication
+print(2*2)
 # #division
+print(3/2)
 # #modulo
+print(7%4) # remainder of two
 # #powers
+print(2**3)
 # #get the max and min of a number
+print(max(1,2)) # max number get the largest number
+print(min(1,2)) # min number get the smallest number
 # #round a number
+print(round(3.4))
 # # absolute value
+print(abs(-3)) # absolute value of a number 
 # # order of operations
+print(2 + 10 * 10 + 3)
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print(floor(3.7)) # floor method rpunds down
 # #ceil method
+#print(ceil())
 # #sqrt method
-
-
-
-
-
-
-
-
-
+#print(sqrt())
 
 
 
@@ -216,15 +220,23 @@ print(f"{number},{number1},{number2},{number3},{number4},{number5}")
 # # **Getting Input from users**
 # #how do we get input from users?
 # input("what is your name?")
+name = input("What is your name?")
+print("hello " + name)
 # # basic math calculator
 # #ask the user for 2 numbers
-# # print out a statement where you:
-# # add them together
-# #multiply
-# # find the max number
-# # find the remainder of the numbers
-# #round one number
-
+# num1 = int(input("enter a number: "))
+# num2 = int(input("enter another number: "))
+# # # print out a statement where you:
+# # # add them together
+# print(num1 + num2)
+# # #multiply
+# print(num1 * num2)
+# # # find the max number
+# print(max(num1,num2))
+# # # find the remainder of the numbers
+# print(num1%num2)
+# # #round one number
+# print(round(num1,num2))
 
 
 
